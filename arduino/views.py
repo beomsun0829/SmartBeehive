2d148fbdcaffe6c81c1c4b4731ea1459ad9f3bf7
beomsun0829 <ghsqjatns@gmail.com>
2024-05-02 12:00:01 +0900

    update post

--- arduino/views.py
+++ arduino/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from datetime import datetime
 
 from .serializers import SoundSensorSerializer, TemperatureSensorSerializer
 from .models import SoundSensor, TemperatureSensor
@@ -12,23 +13,42 @@ class SoundSensorView(APIView):
         return Response(serializer.data)
     
     def post(self, request):
-        serializer = SoundSensorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": "success"}, status=201)
-        else:
-            return Response({"status": "failed"}, status=400)
+        #serializer = SoundSensorSerializer(data=request.data)
+        
+        recorded_at = datetime.strptime(request.data["timestamp"], "%Y-%m-%d %H:%M:%S")
+        sound = request.data["sound"]
+
+        SoundSensor.objects.create(sound=sound, recorded_at=recorded_at).save()
+        return Response({"status": "success"}, status=200)
+
+        # serializer = SoundSensorSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response({"status": "success"}, status=201)
+        # else:
+        #     return Response({"status": "failed"}, status=400)
         
 class TemperatureSensorView(APIView):
     def get(self, request):
         temperature = TemperatureSensor.objects.all()
-        serializer = TemperatureSensorSerializer(temperature, many=True)
+        serializer = TemperatureSensorSerializer(temperature, many=True)    
         return Response(serializer.data)
-    
+
     def post(self, request):
-        serializer = TemperatureSensorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": "success"}, status=201)
-        else:
-            return Response({"status": "failed"}, status=400)
+        #serializer = TemperatureSensorSerializer(data=request.data)
+        
+        recorded_at = datetime.strptime(request.data["timestamp"], "%Y-%m-%d %H:%M:%S")
+        co = request.data["co"]
+        temperature = request.data["temperature"]
+        humidity = request.data["humidity"]
+        nitrogen_dioxide = request.data["nitrogen_dioxide"]
+
+        TemperatureSensor.objects.create(co = co , temperature = temperature , humidity = humidity , nitrogen_dioxide= nitrogen_dioxide , recorded_at=recorded_at).save()
+        return Response({"status": "success"}, status=200)
+
+        # serializer = TemperatureSensorSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response({"status": "success"}, status=201)
+        # else:
+        #     return Response({"status": "failed"}, status=400)
